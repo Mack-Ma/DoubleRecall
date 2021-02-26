a4ab83427269028582300801fbbc9befed2867f9
bobodeligong <48669265+bobodeligong@users.noreply.github.com>
2021-02-26 01:18:43 -0800

    Add Practice Part

--- task_main.js
+++ task_main.js
@@ -1,3 +1,4 @@
+var trialNumberPrac = 6;
 var trialNumber = 8;
 
 // define the site that hosts stimuli images
@@ -10,7 +11,110 @@ repo_site + 'images/stim/3.svg',
 repo_site + 'images/stim/4.svg',
 repo_site + 'images/stim/emotion1.png'];
 
-// randomize color index pairs
+// practice part
+// randomize color index pairs in practice trials
+var colIndexPrac=[];
+      for (m = 0; m<trialNumberPrac; m++){
+      colIndexPrac[m]=[Math.floor(Math.random()*360), Math.floor(Math.random()*360)]
+      };
+// randomize probe locations
+var probLocIndexPrac = [];
+      for (n = 0; n<trialNumberPrac; n++){
+        probLocIndexPrac[n] = Math.floor(Math.random()*2)
+      };
+
+var stimuliOrderPrac = {
+        image:[1,2,3,4],
+        colIndexPrac: colIndexPrac,
+        probLocIndexPrac: probLocIndexPrac,
+      }
+
+var stimuliIndexPrac =[];
+  for (w = 0; w < trialNumberPrac; w++) {
+    stimuliIndexPrac[w] = {
+      stimulus: stimuliOrderPrac.image[w],
+      colIndex: stimuliOrderPrac.colIndexPrac[w],
+      probLocIndex: stimuliOrderPrac.probLocIndexPrac[w],
+    }
+  }
+
+  var timeline = [];
+
+// Instructions for Practice part
+var instructions_practice = {
+	type : 'instructions',
+	pages: ['PRACTICE </br></br> Each image will appear one by one. Study the color of each image. You will be asked to recall the colors later.</br></br>' +
+'Ready? </br></br>'],
+show_clickable_nav: true
+
+};
+timeline.push(instructions_practice);
+
+var fixationWhitePrac = {
+	type: 'html-keyboard-response',
+	stimulus: '<div style="font-size:60px;">+</div>',
+	data: {prac_part: 'fixation'},
+	choices: jsPsych.NO_KEYS,
+	trial_duration: 1000
+
+}
+
+var studyBlockPrac = {
+      type: 'snap-keyboard-response',
+      stimulus: jsPsych.timelineVariable('stimulus'),
+      colIndex: jsPsych.timelineVariable('colIndex'),
+      trial_duration: 1000,
+      choices: jsPsych.NO_KEYS,
+      data: {
+        stimulus: jsPsych.timelineVariable('stimulus'),
+        colIndex: jsPsych.timelineVariable('colIndex'),
+      }
+  };
+
+  var delayPrac ={
+    type: 'html-keyboard-response',
+    stimulus: '<div style="font-size:60px;">+</div>',
+    data: {prac_part: 'delay'},
+    choices: jsPsych.NO_KEYS,
+    trial_duration: 1000
+  }
+  
+  var testBlockPrac = {
+      type: 'continuous_report',
+      stimulus: jsPsych.timelineVariable('stimulus'),
+      colIndex: jsPsych.timelineVariable('colIndex'),
+      probLocIndex: jsPsych.timelineVariable('probLocIndex'),
+      stim_duration: -1,
+      data: {
+        stimulus: jsPsych.timelineVariable('stimulus'),
+        colIndex: jsPsych.timelineVariable('colIndex'),
+        probLocIndex: jsPsych.timelineVariable('probLocIndex'),
+      }
+  };
+  
+  var doubleTestBlockPrac = {
+    type: 'continuous_report_double',
+    stimulus: jsPsych.timelineVariable('stimulus'),
+    colIndex: jsPsych.timelineVariable('colIndex'),
+    probLocIndex: jsPsych.timelineVariable('probLocIndex'),
+    stim_duration: -1,
+    data: {
+      stimulus: jsPsych.timelineVariable('stimulus'),
+      colIndex: jsPsych.timelineVariable('colIndex'),
+      probLocIndex: jsPsych.timelineVariable('probLocIndex'),
+    }
+  };
+
+  var prac_procedure = {
+    timeline: [fixationWhitePrac, studyBlockPrac, delayPrac, testBlockPrac, doubleTestBlockPrac],
+    //timeline: [emotion_induction, fixationWhite, studyBlock, delay, testBlock],
+    timeline_variables: stimuliIndexPrac,
+
+}
+timeline.push(prac_procedure);
+
+// Main experiment part
+      // randomize color index pairs
 var colIndex=[];
       for (j = 0; j<trialNumber; j++){
       colIndex[j]=[Math.floor(Math.random()*360), Math.floor(Math.random()*360)]
@@ -80,7 +184,7 @@ var stimuliIndex =[];
   }
 
 /* create timeline */
-var timeline = [];
+// var timeline = [];
 
 // Instructions for Study Task
 var instructions_study = {
