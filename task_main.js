63581d2489a454aa8d19c67970c040d9f395420c
bobodeligong <48669265+bobodeligong@users.noreply.github.com>
2021-02-26 18:22:53 -0800

    Update task_main.js

--- task_main.js
+++ task_main.js
@@ -9,8 +9,8 @@ var all_images = [repo_site + 'images/stim/1.svg',
 //repo_site + 'images/stim/2.svg',
 //repo_site + 'images/stim/3.svg',
 //repo_site + 'images/stim/4.svg',
-//repo_site + 'images/stim/emotion1.png',
-repo_site + 'images/stim/studySample.png',
+repo_site + 'images/stim/emotion1.png',
+//repo_site + 'images/stim/studySample.png',
 //repo_site + 'images/stim/probeLeftSample.png',
 //repo_site + 'images/stim/probeRightSample.png'
 ];
